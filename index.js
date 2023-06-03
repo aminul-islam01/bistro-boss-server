5860f78a214aab90732a78e9460370826a7aa4f5
aminul-islam01 <abiddut953@gmail.com>
2023-06-03 13:13:15 +0600

    add payment gateway

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -159,6 +160,24 @@ async function run() {
       const result = cartsCollection.deleteOne(query);
       res.send(result)
     })
+
+    // create payment intent
+    app.post("/create-payment-intent", verifyJWT, async(req, res) => {
+      const { price } = req.body;
+      const amount = price*100;
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ['card']
+        // automatic_payment_methods: {
+        //   enabled: true,
+        // },
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
